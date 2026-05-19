98e0144c9efefdb86f9bb45973d16f7b0224a544
LeBaptouBaptiste <153990224+LeBaptouBaptiste@users.noreply.github.com>
2026-05-19 10:49:16 +0200

    Update Conf Stripe

---
+++ API_Althea-systems/Migrations/20260519084501_AddStripeCustomerIdToUser.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API_Althea_systems.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStripeCustomerIdToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "StripeCustomerId",
+                table: "users",
+                type: "character varying(255)",
+                maxLength: 255,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StripeCustomerId",
+                table: "users");
+        }
+    }
+}
